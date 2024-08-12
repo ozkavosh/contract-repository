d889040c5809ad88279c367e2e6462081dedb39a
Ozkavosh <augustoslva@hotmail.com>
2024-08-12 18:29:15 -0300

    Add dotenv dependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as env } from "dotenv";
+
+env();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
